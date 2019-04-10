6391b56963987b781a2e5dde05cbfc203e16977c
Mels <melseck18@gmail.com>
2019-04-11 00:35:02 +0200

    final check done

---
+++ Homework/week_1/moviescraper.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# Name:mels van eck
+# Student number:12505757
+"""
+This script scrapes IMDB and outputs a CSV file with highest rated movies.
+"""
+
+import csv
+from requests import get
+from requests.exceptions import RequestException
+from contextlib import closing
+from bs4 import BeautifulSoup
+
+
+TARGET_URL = "https://www.imdb.com/search/title?title_type=feature&release_date=2008-01-01,2018-01-01&num_votes=5000,&sort=user_rating,desc"
+BACKUP_HTML = 'movies.html'
+OUTPUT_CSV = 'movies.csv'
+
+
+def extract_movies(dom):
+    """
+    Extract a list of highest rated movies from DOM (of IMDB page).
+    Each movie entry should contain the following fields:
+    - Title
+    - Rating
+    - Year of release (only a number!)
+    - Actors/actresses (comma separated if more than one)
+    - Runtime (only a number!)
+    """
+    movies = []
+    # Loop through the html-page and put all data per movie in a dictionary
+    for title in dom.findAll("div", {"class": "lister-item mode-advanced"}):
+        movie = {}
+        movie["Title"] = title.h3.a.text
+        for rating in title.findAll("span", {"class": "value"}):
+            movie["Rating"] = float(rating.text)
+        for year in title.findAll("span",
+                          {"class": "lister-item-year text-muted unbold"}):
+            year = year.text
+            year = year.replace('(', '').replace(')', '').replace('II', '')\
+                        .replace('I', '')
+            year = int(year)
+            movie["Year"] = year
+        actors = []
+        for actor in title.select('a[href*=_st]'):
+            actors.append(actor.text)
+        actorsstring = "/".join(actors)
+        movie["Actors"] = actorsstring
+        for runtime in title.findAll("span", {"class": "runtime"}):
+            runtime = (runtime.text).replace("min", "")
+            movie["Runtime"] = runtime
+        # append all the movie dictionaries to a list
+        movies.append(movie)
+
+    return movies
+
+
+def save_csv(outfile, movies):
+    """
+    Output a CSV file containing highest rated movies.
+    """
+    # make the first row for the collumn names
+    csv_columns = ['Title', 'Rating', 'Year', 'Actors', 'Runtime']
+
+    # write the list of the movies to the csv file
+    try:
+        with open(outfile, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in movies:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
+
+def simple_get(url):
+    """
+    Attempts to get the content at `url` by making an HTTP GET request.
+    If the content-type of response is some kind of HTML/XML, return the
+    text content, otherwise return None
+    """
+    try:
+        with closing(get(url, stream=True)) as resp:
+            if is_good_response(resp):
+                return resp.content
+            else:
+                return None
+    except RequestException as e:
+        print('The following error occurred during HTTP GET request\
+               to {0} : {1}'.format(url, str(e)))
+        return None
+
+
+def is_good_response(resp):
+    """
+    Returns true if the response seems to be HTML, false otherwise
+    """
+    content_type = resp.headers['Content-Type'].lower()
+    return (resp.status_code == 200
+            and content_type is not None
+            and content_type.find('html') > -1)
+
+
+if __name__ == "__main__":
+
+    # get HTML content at target URL
+    html = simple_get("https://www.imdb.com/search/title?title_type=feature&release_date=2008-01-01,2018-01-01&num_votes=5000,&sort=user_rating,desc")
+
+    # save a copy to disk in the current directory, this serves as an backup
+    # of the original HTML, will be used in grading.
+    with open(BACKUP_HTML, 'wb') as f:
+        f.write(html)
+    # parse the HTML file into a DOM representation
+    dom = BeautifulSoup(html, 'html.parser')
+
+    # extract the movies (using the function you implemented)
+    movies = extract_movies(dom)
+
+    save_csv("movies.csv", movies)
---
+++ Homework/week_1/visualizer.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python
+# Name:
+# Student number:
+"""
+This script visualizes data obtained from a .csv file
+"""
+
+import csv
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+from matplotlib.ticker import FormatStrFormatter
+import numpy as np
+
+# Global constants for the input file, first and last year
+INPUT_CSV = "movies.csv"
+
+def make_plot(movies):
+
+    averageRate = {}
+    allrates = []
+    new = {}
+
+    # loop through all the movies
+    for movie in movies:
+        # Make a list of all the ratings for a total average
+        allrates.append(movies[movie]["Rating"])
+        # make a dict of all the years in wich the movies where made as keys
+        # the values are all the ratings in that year
+        if movies[movie]["Year"] not in averageRate:
+            averageRate[movies[movie]["Year"]] = [movies[movie]["Rating"]]
+        else:
+            averageRate[movies[movie]["Year"]].append(movies[movie]["Rating"])
+    # sort the dictionary chronologically in years
+    averageRate = sorted(averageRate.items(), key=lambda x: x[0])
+    averageRate = dict(averageRate)
+
+    # calculate the total average
+    totalavg = average(allrates)
+    totalavg = round(totalavg,2)
+
+    # make it compatible for the plot
+    allrates = [totalavg] * len(averageRate)
+
+    # make the an average number of the rating list
+    for year in averageRate:
+        ratings = averageRate[year]
+        averageRate[year] = average(ratings)
+        averageRate[year] = round(averageRate[year], 2)
+
+
+
+    plt.figure(figsize=(12,4))
+    plt.subplot(121)
+    plt.plot(range(len(averageRate )), list(averageRate .values()))
+    plt.xticks(range(len(averageRate )), list(averageRate .keys()))
+    plt.yticks(np.arange(0, 11, 1.0))
+    plt.ylabel('ratings')
+    plt.xlabel('years')
+    plt.grid(True)
+
+    plt.subplot(122)
+    red_patch = mpatches.Patch(color='red', label='yearly avg.')
+    blue_patch = mpatches.Patch(color='blue', label='total avg.')
+    plt.legend(handles=[red_patch,blue_patch])
+    plt.scatter(range(len(averageRate)), list(averageRate.values()), color="red")
+    plt.plot(range(len(averageRate)), allrates)
+    plt.xticks(range(len(averageRate)), list(averageRate.keys()))
+    plt.yticks(np.arange(7, 10, 0.3))
+    plt.xlabel('years')
+    plt.ylabel('ratings')
+    plt.grid(True)
+
+    plt.show()
+
+# average calculator
+def average(lst):
+    return sum(lst) / len(lst)
+
+if __name__ == "__main__":
+
+    movies = {}
+    # read the csv file and make it a dictionary
+    with open(INPUT_CSV, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            movie = {}
+            movie["Year"] = int(row["Year"])
+            movie["Rating"] = float(row["Rating"])
+            movie["Runtime"] = row["Runtime"]
+            movie["Actors"] = row["Actors"]
+            movies[row["Title"]] = movie
+
+    make_plot(movies)
